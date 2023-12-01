d6575691003b1649f7d27bfc84d03c0128b34421
Zach <142842209+Zachermann@users.noreply.github.com>
2023-12-01 14:12:38 -0500

    Update bakers.cpp

--- bakers.cpp
+++ bakers.cpp
@@ -1,190 +1,81 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include <iostream>
+#include <vector>
 
-#define MAX_PROCESSES 5
-#define MAX_RESOURCES 3
+using namespace std;
 
-int available[MAX_RESOURCES];
-int maximum[MAX_PROCESSES][MAX_RESOURCES];
-int allocation[MAX_PROCESSES][MAX_RESOURCES];
-int need[MAX_PROCESSES][MAX_RESOURCES];
+const int NUM_PROCESSES = 5;
+const int NUM_RESOURCES = 3;
 
-void initializeSystem() {
-    // Initialize available resources
-    printf("Enter the number of instances for each resource:\n");
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        printf("Resource %d: ", j);
-        scanf("%d", &available[j]);
-    }
+bool isSafeState(const vector<vector<int>>& max, const vector<vector<int>>& alloc, const vector<int>& avail, vector<int>& safeSeq) {
+    vector<int> work = avail;
+    vector<bool> finish(NUM_PROCESSES, false);
 
-    // Initialize maximum claims
-    printf("\nEnter the maximum claims for each process and resource:\n");
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        for (int j = 0; j < MAX_RESOURCES; ++j) {
-            printf("Process %d, Resource %d: ", i, j);
-            scanf("%d", &maximum[i][j]);
-            need[i][j] = maximum[i][j];
-        }
-    }
-
-    // Initialize allocation matrix
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        for (int j = 0; j < MAX_RESOURCES; ++j) {
-            allocation[i][j] = 0;
-        }
-    }
-}
-
-void displaySystemState() {
-    printf("\nSystem State:\n");
-    printf("Available: ");
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        printf("%d ", available[j]);
-    }
-    printf("\n");
-
-    printf("Maximum:\n");
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        for (int j = 0; j < MAX_RESOURCES; ++j) {
-            printf("%d ", maximum[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("Allocation:\n");
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        for (int j = 0; j < MAX_RESOURCES; ++j) {
-            printf("%d ", allocation[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("Need:\n");
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        for (int j = 0; j < MAX_RESOURCES; ++j) {
-            printf("%d ", need[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-bool isSafeState(int requestProcess, int request[]) {
-    // Check if request is within need and available
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        if (request[j] > need[requestProcess][j] || request[j] > available[j]) {
-            return false;
-        }
-    }
-
-    // Simulate resource allocation and check for safety
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        available[j] -= request[j];
-        allocation[requestProcess][j] += request[j];
-        need[requestProcess][j] -= request[j];
-    }
-
-    // Check if the system is still in a safe state
-    bool finish[MAX_PROCESSES] = {false};
-    int work[MAX_RESOURCES];
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        work[j] = available[j];
-    }
-
-    bool changed;
-    do {
-        changed = false;
-        for (int i = 0; i < MAX_PROCESSES; ++i) {
+    for (int k = 0; k < NUM_PROCESSES; ++k) {
+        bool found = false;
+        for (int i = 0; i < NUM_PROCESSES; ++i) {
             if (!finish[i]) {
-                bool canFinish = true;
-                for (int j = 0; j < MAX_RESOURCES; ++j) {
-                    if (need[i][j] > work[j]) {
-                        canFinish = false;
+                bool canAllocate = true;
+                for (int j = 0; j < NUM_RESOURCES; ++j) {
+                    if (max[i][j] - alloc[i][j] > work[j]) {
+                        canAllocate = false;
                         break;
                     }
                 }
 
-                if (canFinish) {
-                    finish[i] = true;
-                    for (int j = 0; j < MAX_RESOURCES; ++j) {
-                        work[j] += allocation[i][j];
+                if (canAllocate) {
+                    for (int j = 0; j < NUM_RESOURCES; ++j) {
+                        work[j] += alloc[i][j];
                     }
-                    changed = true;
+                    safeSeq.push_back(i);
+                    finish[i] = true;
+                    found = true;
                 }
             }
         }
-    } while (changed);
 
-    // Restore the state for future checks
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        available[j] += request[j];
-        allocation[requestProcess][j] -= request[j];
-        need[requestProcess][j] += request[j];
-    }
-
-    // Check if all processes finish
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        if (!finish[i]) {
+        if (!found) {
+            // If no process can be allocated, the system is not in a safe state
             return false;
         }
     }
 
+    // If all processes are finished, the system is in a safe state
     return true;
 }
 
-void requestResources(int requestProcess, int request[]) {
-    if (isSafeState(requestProcess, request)) {
-        for (int j = 0; j < MAX_RESOURCES; ++j) {
-            available[j] -= request[j];
-            allocation[requestProcess][j] += request[j];
-            need[requestProcess][j] -= request[j];
-        }
-        printf("Request granted.\n");
-    } else {
-        printf("Request denied. System will be in an unsafe state.\n");
-    }
-}
-
-void releaseResources(int releaseProcess, int release[]) {
-    for (int j = 0; j < MAX_RESOURCES; ++j) {
-        available[j] += release[j];
-        allocation[releaseProcess][j] -= release[j];
-        need[releaseProcess][j] += release[j];
-    }
-}
-
 int main() {
-    initializeSystem();
-    displaySystemState();
+    // Given snapshot at time t0
+    vector<vector<int>> max = {
+        {7, 5, 3},
+        {3, 2, 2},
+        {9, 0, 2},
+        {2, 2, 2},
+        {4, 3, 3}
+    };
 
-    int choice, process, request[MAX_RESOURCES], release[MAX_RESOURCES];
+    vector<vector<int>> alloc = {
+        {0, 1, 0},
+        {2, 0, 0},
+        {3, 0, 2},
+        {2, 1, 1},
+        {0, 0, 2}
+    };
 
-    do {
-        printf("\nEnter command (1 = Request, 2 = Release, 0 = Exit): ");
-        scanf("%d", &choice);
+    vector<int> avail = {3, 3, 2};
+    vector<int> safeSeq;
 
-        switch (choice) {
-            case 1:
-                printf("Enter process number for request: ");
-                scanf("%d", &process);
+    if (isSafeState(max, alloc, avail, safeSeq)) {
+        cout << "The system is in a safe state.\nSafe sequence: ";
+        for (int i = 0; i < safeSeq.size(); ++i) {
+            cout << "P" << safeSeq[i];
+            if (i != safeSeq.size() - 1) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+    } else {
+        cout << "The system is not in a safe state.\n";
+    }
 
-                printf("Enter resource request: ");
-                for (int j = 0; j < MAX_RESOURCES; ++j) {
-                    scanf("%d", &request[j]);
-                }
-
-                requestResources(process, request);
-                break;
-
-            case 2:
-                printf("Enter process number for release: ");
-                scanf("%d", &process);
-
-                printf("Enter resource release: ");
-                for (int j = 0; j < MAX_RESOURCES; ++j) {
-                    scanf("%d", &release[j]);
-                }
-
-                releaseResources(process, release);
-                break;
-
-            case 0:
+    return 0;
+}
